a1a8473bc72fce7c70980c290b705965e86a338f
Archit Singh <contact.architsingh@gmail.com>
2023-10-29 05:02:56 +0530

    new: add init

--- be/app.py
+++ be/app.py
@@ -4,16 +4,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-def generate_events():
-    while True:
-        prompt = request.args.get('prompt')
+def generate_events(input_text):
+    event_data = f"received:\n {input_text*100}\n\n"
+    for char in event_data:
+        yield char.encode('utf-8')
 
-        event_data = f"data: {prompt}\n\n"
-        yield event_data
-
-@app.route('/chat', methods=['GET'])
-def chat():
-    return Response(generate_events(), content_type='text/event-stream')
+@app.route('/chat', methods=['POST'])
+def completion_api():
+    data = request.get_json()
+    input_text = data['key']
+    return Response(generate_events(input_text), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,52 +1,25 @@
-import React, { useState } from "react";
-import Chat, { Bubble, useMessages } from "@chatui/core";
-import "@chatui/core/dist/index.css";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  const { messages, appendMsg, setTyping } = useMessages([]);
-  const [cont, setcont] = useState("11111");
-
-  function handleSend(type, val) {
-    if (type === "text" && val.trim()) {
-      appendMsg({
-        type: "text",
-        content: { text: val },
-        position: "right"
-      });
-
-      // Modify the Axios request to be a POST request with the desired body and headers.
-      axios
-        .post("http://localhost:3000/chat", { key: val }, {
-          headers: { "Content-Type": "application/json", "Accept": "text/event-stream" }
-        })
-        .then((res) => {
-          console.log(res.data);
-          console.log("data", cont);
-          setcont(res.data);
-          setTimeout(() => {
-            console.log("data_now", cont);
-            appendMsg({
-              type: "text",
-              content: { text: res.data }
-            });
-          }, 1000);
-        });
-      setTyping(true);
-    }
-  }
-
-  function renderMessageContent(msg) {
-    const { content } = msg;
-    return <Bubble content={content.text} />;
-  }
-
+function App() {
   return (
-    <Chat
-      navbar={{ title: "AI" }}
-      messages={messages}
-      renderMessageContent={renderMessageContent}
-      onSend={handleSend}
-    />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
